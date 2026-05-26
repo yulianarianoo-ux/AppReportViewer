b76b116ffd93f8e4764d269e58d51c96a5c11aca
Esmeralda Obregon <yuliana.rianoo@colpre.edu.co>
2026-05-25 23:18:57 -0500

    ewuitfhyi vertrdebhtfidredgljt

--- AppReportViewer/Logica/Persona.cs
+++ AppReportViewer/Logica/Persona.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AppReportViewer.Datos;
+using AppReportViewer.Modelo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +9,10 @@ namespace AppReportViewer.Logica
 {
     public class Persona
     {
+        public List<Producto> MtListaProductos()
+        {
+            ProductoD oProductoD = new ProductoD();
+            return oProductoD.MtListaProductos();
+        }
     }
 }
