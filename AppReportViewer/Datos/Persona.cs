e32cc307710b87e9bfded6fa1d00d6c1f26593ba
Esmeralda Obregon <yuliana.rianoo@colpre.edu.co>
2026-05-25 23:15:55 -0500

    swerftyuer

--- AppReportViewer/Datos/Persona.cs
+++ AppReportViewer/Datos/Persona.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using AppReportViewer.Modelo;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,41 @@ namespace AppReportViewer.Datos
 {
     public class Persona
     {
+        public List<Producto> MtListaProductos()
+        {
+            List<Producto> listaProductos = new List<Producto>();
+
+            using (SqlConnection cn = ConexionDB.AbrirConexion())
+            {
+                cn.Open();
+
+                string consulta = @"SELECT p.IdProducto, p.NombreProducto, p.Precio, p.Stock, c.NombreCategoria 
+                             FROM Productos p 
+                             INNER JOIN Categorias c ON p.IdCategoria = c.IdCategoria";
+
+                using (SqlCommand cmd = new SqlCommand(consulta, cn))
+                {
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            listaProductos.Add(new Producto
+                            {
+                                Id = Convert.ToInt32(dr["IdProducto"]),
+                                NombreProducto = dr["NombreProducto"].ToString(),
+                                Precio = Convert.ToDecimal(dr["Precio"]),
+                                Stock = Convert.ToInt32(dr["Stock"]),
+                                NombreCategoria = dr["NombreCategoria"].ToString()
+                            });
+
+                        }
+                    }
+                }
+
+            }
+            return listaProductos;
+        }
+    }
+}
     }
 }
